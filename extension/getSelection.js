b6b382f8ea47cd1a38a64b1a229119a7b739be88
Patrick Scherer <patrick.scherer@shopgate.com>
2024-02-01 15:26:51 +0100

    Removed catch blocks in the pipeline requests

--- extension/getSelection.js
+++ extension/getSelection.js
@@ -1,34 +1,27 @@
 module.exports = async (context) => {
-  try {
-    let selection;
+  let selection;
 
-    // get selection from cache
-    const cachedSelection = await context.storage.device.get('selection');
-    const isConfigured = context.config.pageLinking.some(
-      ({ path }) => (cachedSelection ? path === cachedSelection.path : false)
-    );
+  // get selection from cache
+  const cachedSelection = await context.storage.device.get('selection');
+  const isConfigured = context.config.pageLinking.some(
+    ({ path }) => (cachedSelection ? path === cachedSelection.path : false)
+  );
 
-    // first start or the cached selection is not configured
-    if (!cachedSelection || !isConfigured) {
-      selection = context.config.pageLinking.find(({ path }) => path === '/');
+  // first start or the cached selection is not configured
+  if (!cachedSelection || !isConfigured) {
+    selection = context.config.pageLinking.find(({ path }) => path === '/');
 
-      // missing or wrong config
-      if (!selection) {
-        selection = {
-          label: null,
-          categoryId: null,
-          path: '/',
-        };
-      }
-    } else {
-      selection = cachedSelection;
-      context.log.info('New app start with cached selection');
+    // missing or wrong config
+    if (!selection) {
+      selection = {
+        label: null,
+        categoryId: null,
+        path: '/',
+      };
     }
-
-    return { selection };
-  } catch (err) {
-    context.log.error(err, 'Selection from cache could not be retrieved');
-
-    throw err;
+  } else {
+    selection = cachedSelection;
   }
+
+  return { selection };
 };
--- extension/setSelection.js
+++ extension/setSelection.js
@@ -1,10 +1,3 @@
 module.exports = async (context, { newSelection }) => {
-  try {
-    await context.storage.device.set('selection', newSelection);
-    context.log.info(`New selection is '${newSelection.path}'`);
-  } catch (err) {
-    context.log.error(err, 'Failed to set new selection');
-
-    throw err;
-  }
+  await context.storage.device.set('selection', newSelection);
 };
