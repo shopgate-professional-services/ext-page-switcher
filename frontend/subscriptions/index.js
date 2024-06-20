7dd5f11e84179ccfe03c8cd92643392afd6fd2aa
Frithjof Klös <frithjof.kloes@shopgate.com>
2024-06-20 09:00:30 +0200

    CURB-4199 Improved generation of the initial route

--- frontend/subscriptions/index.js
+++ frontend/subscriptions/index.js
@@ -10,19 +10,37 @@ import {
 import { getSelection } from '../selectors';
 import { SET_SELECTION } from '../constants';
 
+const baseUrlRegex = /https?:\/\/(?:(?:(?:[0-9]{1,3}\.){3}[0-9]{1,3}(?::[0-9]+)?)|(?:.+index.html))/gm;
+
+/**
+ * Helper to retrieve the base url of the webview
+ * @returns {string}
+ */
+const getBaseUrl = () => {
+  const [basedUrl = ''] = window.location.href.match(baseUrlRegex) || [];
+  return basedUrl;
+};
+
 export default (subscribe) => {
   const setSwitchSelection$ = main$.filter(({ action }) => action.type === SET_SELECTION);
 
   subscribe(appWillInit$, ({ getState }) => {
+    /**
+     * Register an appInitialization callback that takes care that initial route of the app
+     * is always the path from the selection.
+     */
     appInitialization.set('ext-page-switcher', () => {
       const selection = getSelection(getState());
       if (selection?.path !== INDEX_PATH) {
-        window.history.replaceState({}, null, selection.path);
+        window.history.replaceState({}, null, `${getBaseUrl()}${selection.path}`);
       }
     });
   });
 
   subscribe(appWillStart$, ({ getState }) => {
+    /**
+     * Register a redirect for the index path that always leads to the path of the current selection
+     */
     redirects.set(INDEX_PATH, () => {
       const selection = getSelection(getState());
 
@@ -38,3 +56,4 @@ export default (subscribe) => {
     }));
   });
 };
+
