46eeba8f2a584a0baff6a7fb17b38b6d880106e6
aylinuenal <aylin.uenal@shopgate.com>
2025-04-01 10:35:16 +0200

    CURB-4954 adjust category tree for browse page, bump version 1.4.0

--- frontend/components/HideBackArrow/connector.js
+++ frontend/components/HideBackArrow/connector.js
@@ -5,7 +5,6 @@ import {
 
 /**
  * Maps the contents of the state to the component props.
- * @param {Object} state The current application state.
  * @return {Object} The extended component props.
  */
 const makeMapStateToProps = () => {
